8fb4b48a7b92aaa616a9f6ddd43d7e1de31184b0
6704062611379 <s6704062611379@email.kmutnb.ac.th>
2024-11-28 15:08:56 +0700

    Update 2

--- lab1_min_max.cpp
+++ lab1_min_max.cpp
@@ -1,10 +1,11 @@
 int main() {
-	int num1=5;
-	int num2=10;
+	int num1;
+	int num2;
 	if (num1>num2) {
-		printf("num1 greater than num2");
-	} else {
-		printf("num2 greater than num1");
+		printf("num1 greater than num2")
+		print("%d\n",num1-num2)
+	} eles {
+		printf("num2 greater than num1")
 	}
 	
 }
