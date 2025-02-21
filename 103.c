5130c22e15baf2498b9b9b77631dd85b07d2934f
T.Natnicha <s6704062611379@email.kmutnb.ac.th>
2025-02-21 16:13:46 +0700

    Add files via upload

--- 103.c
+++ 103.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> // * = เน้น
 int coffeeCS(int A[], char m[][15], int p[], int N);
-main(){
+int main(){
 	char m[][15] = {"Latte","Espresso","Iris","Frappuccino","Cappuccino","Mocha","Americano","Black Eye"};
 	int p[] = {35,45,30,50,45,48,39,35};
 	int n,i;
@@ -12,11 +12,32 @@ main(){
 	coffeeCS(a,m,p,n);
 }
 int coffeeCS(int A[], char m[][15], int p[], int N){
-	int i;
+	int i,j, max=0, sum=0;
 	int mo[2][8]={{1,2,3,4,5,6,7,8}
-					,{0,0,0,0,0,0,0,0}};
-	printf("%s",m[mo[0][0]-1]);
-	/*for(i=0;i<N;i++){
-		
-	}*/
+				 ,{0,0,0,0,0,0,0,0}};
+	//printf("%s",m[mo[0][0]-1]);
+	for(i=0;i<N;i++){ //****** การเปรียบเทียบ
+		for(j=0;j<8;j++){
+			if(A[i] == mo[0][j]){
+				mo[1][j]++;
+			}
+		}
+	}
+	for(i=0;i<8;i++){
+		printf("%s(%d)\n", m[mo[0][i]-1], p[i]*mo[1][i]);//******
+	}
+	for(i=0;i<N;i++){
+		sum = sum + p[A[i]-1];//******
+	}
+	printf("Total: %d\n", sum);
+	for(i=0;i<8;i++){//******
+		if(mo[1][i]>max){
+			max = mo[1][i]; //******
+		}
+	}
+	for(i=0;i<8;i++){//******
+		if(max == mo[1][i]){
+			printf("Max list coffee: %s(%d)", m[mo[0][i]-1], max);
+		}
+	}
 }
